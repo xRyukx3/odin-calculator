44b1976d334817ccbe669af8526ecb4651d4cd3e
David <96595953+xRyukx3@users.noreply.github.com>
2025-10-13 11:05:17 -0400

    Define math basic functions and initialize the operation object

---
+++ script.js
@@ -0,0 +1,21 @@
+let operation = {
+  firstNumber: 0,
+  secondNumber: 0,
+  operator: "+",
+};
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
